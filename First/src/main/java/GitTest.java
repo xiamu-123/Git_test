039b05283d3b280c9a5d423b4663d1ee486fbc05
xiamu-123 <1539434945@qq.com>
2023-03-12 19:14:02 +0800

    First Commit

---
+++ First/src/main/java/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * @ProjectName: Git_test
+ * @Package: PACKAGE_NAME
+ * @ClassName: GitTest
+ * @Author: FH
+ * @Date: 2023/3/12 19:05
+ * @Version: 1.0
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
